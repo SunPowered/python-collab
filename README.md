0e0dca373a758dbaa66766cc97d1d7816b8132ee
Tim van Boxtel <tim@vanboxtel.ca>
2015-07-09 11:34:06 -0400

    Bugfix parsing args, Spyder files in gitignore spelled incorrectly

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This project is intended to be used as a collaborative project when learning Python and Git
+
+Changes added
--- pysines/main.py
+++ pysines/main.py
@@ -4,18 +4,28 @@
 import sys
 import argparse
 
+def check_options(options):
+    """
+    Check that the options are sane and restricted to the project specs
+    :param options: An `argparse.Namespace` object
+    :return:
+    """
+
 def create_parser():
     """
     Create the Argument Parser for this script
-    :return:  An argparse.ArgumentParser instance
+    :return:  An `argparse.ArgumentParser` instance
     """
     parser = argparse.ArgumentParser('pysines', description="")
+    parser.add_argument('-f', '--file', help="A file to save")
+    
     return parser
 
 def parse_args(args):
     """
     Perform the actual argument parsing, useful for testing the parser
-    :return: An argparse.Options namespace
+    :param args: The list of arguments to parse, something like `sys.argv`
+    :return: An `argparse.Namespace` object
     """
     parser = create_parser()
     return parser.parse_args(args)
@@ -23,9 +33,12 @@ def parse_args(args):
 def run(options):
     """
     Run the actual script
-    :param options: Parsed options namespace
+    :param options: An `argparse.Namespace` object
     :return:
     """
+
+    # Do some additional check on the options
+    check_options(options)
 
     pass
 
@@ -33,5 +46,6 @@ def run(options):
 if __name__ == '__main__':
     # Script has been launched
 
-    options = parse_args(sys.argv)
+    parser = create_parser()
+    options = parser.parse_args()
     run(options)
